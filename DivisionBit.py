e86714b53077a962808520bdefdc3ba69c256164
KinoshitaYstr <control.division.cyber.crime@gmail.com>
2017-12-17 14:13:27 +0900

    復元できた

--- DivisionBit.py
+++ DivisionBit.py
@@ -5,63 +5,56 @@ class DivisionBit:
     def __init__(self,array):
         self.array = array
         (self.y,self.x) = array.shape
-        self.first = np.zeros((self.y,self.x))
-        self.second = np.zeros((self.y,self.x))
-        self.third = np.zeros((self.y,self.x))
-        self.forth = np.zeros((self.y,self.x))
-        self.fifth = np.zeros((self.y,self.x))
-        self.sixth = np.zeros((self.y,self.x))
-        self.seventh = np.zeros((self.y,self.x))
-        self.eighth = np.zeros((self.y,self.x))
+        self.flat = []
     def deivide(self):
-        self.first = np.where(self.array & 0b1,1,0)
-        self.second = np.where(self.array & 0b10,1,0)
-        self.third = np.where(self.array & 0b100, 1, 0)
-        self.forth = np.where(self.array & 0b1000, 1, 0)
-        self.fifth = np.where(self.array & 0b10000,1,0)
-        self.sixth = np.where(self.array & 0b100000,1,0)
-        self.seventh = np.where(self.array & 0b1000000,1,0)
-        self.eighth = np.where(self.array & 0b10000000,1,0)
+        flag = 0b1
+        for i in range(8):
+            self.flat.append(np.where(self.array & flag,1,0))
+            flag = flag<<1
     def show_all(self):
-        Image.fromarray(self.first * 255).show()
-        Image.fromarray(self.second * 255).show()
-        Image.fromarray(self.third * 255).show()
-        Image.fromarray(self.forth * 255).show()
-        Image.fromarray(self.fifth * 255).show()
-        Image.fromarray(self.sixth * 255).show()
-        Image.fromarray(self.seventh * 255).show()
-        Image.fromarray(self.eighth * 255).show()
+        for i in range(8):
+            Image.fromarray(self.flat[i]*255).show()
+    def show(self,i):
+        Image.fromarray(self.flat[i]*255).show()
     def match(self):
         self.array = np.zeros((self.y,self.x))
-        self.array = self.array+self.first*0b1
-        self.array = self.array+self.second*0b10
-        self.array = self.array+self.third*0b100
-        self.array = self.array+self.forth*0b1000
-        self.array = self.array+self.fifth*0b10000
-        self.array = self.array+self.sixth*0b100000
-        self.array = self.array+self.seventh*0b1000000
-        self.array = self.array+self.eighth*0b10000000
+        flag = 0b1
+        for i in range(8):
+            self.array = self.array+self.flat[i]*flag
+            flag = flag<<1
         Image.fromarray(self.array).show()
     def set_flat(self):
-        flat_array = self.first.reshape(-1,)
-        print("----------------------------")
-        print(flat_array.shape)
-        print(flat_array)
-        print("----------------------------")
-        flat_array = np.append(flat_array, self.second.reshape(-1,))
-        flat_array = np.append(flat_array, self.third.reshape(-1,))
-        flat_array = np.append(flat_array, self.forth.reshape(-1,))
-        flat_array = np.append(flat_array, self.fifth.reshape(-1,))
-        flat_array = np.append(flat_array, self.sixth.reshape(-1,))
-        flat_array = np.append(flat_array, self.seventh.reshape(-1,))
-        flat_array = np.append(flat_array, self.eighth.reshape(-1,))
-        print(flat_array)
-        flat_array = np.append(flat_array,flat_array)
-        print("flat")
-        print(flat_array.shape)
-        print(self.y)
-        print(self.y*self.x)
-        print(self.array.shape)
-        self.array = flat_array.reshape((self.y*4,self.x*4))
-        Image.fromarray(self.array*255).show()
-        
+        flat = []
+        for i in range(8):
+            flat.append(self.flat[i].reshape(-1,))
+        (size,) = flat[0].shape
+        print(size)
+        array = np.array([])
+        for i in range(size):
+            for j in range(8):
+                array = np.append(array,flat[j][i])
+        array = np.append(array,array)
+        print(array.shape)
+        self.array = array.reshape((self.y * 4, self.x * 4))
+        Image.fromarray(self.array * 255).show()
+    def reset_flat(self):
+        array = self.array.reshape(-1,)
+        size, = array.shape
+        size = int(size/2)
+        array = array[:size]
+        bit = [
+            np.array([]), np.array([]), np.array([]), np.array([]),
+            np.array([]), np.array([]), np.array([]), np.array([])
+        ]
+        print(size)
+        for i in range(int(size/8)):
+            for j in range(8):
+                bit[j] = np.append(bit[j],array[i * 8 + j])
+        for i in range(8):
+            bit[i] = bit[i].reshape((int(np.sqrt(size / 8)), int(np.sqrt(size / 8))))
+        self.flat = np.array(bit)
+    def save(self,fname):
+        img = Image.fromarray(self.array)
+        if img.mode != "RGB":
+            img = img.convert("RGB")
+        img.save(fname)
--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from PIL import Image
 from DivisionBit import DivisionBit
 
 def main():
-    img = Image.open("data/Lenna.png").convert("L")
+    img = Image.open("data/Lenna.png").convert("L").resize((50,50))
     img_array1 = np.asarray(img)
     DB1 = DivisionBit(img_array1)
     DB1.deivide()
     DB1.match()
     DB1.set_flat()
-
+    DB1.reset_flat()
+    DB1.match()
+    DB1.save("result/flat1.png")
 if __name__ == "__main__":
     main()
