6d99e1ee1f9bc67ecc0e70033594ac0a1605c004
KinoshitaYstr <control.division.cyber.crime@gmail.com>
2017-12-17 21:34:00 +0900

    yobunano kesita

--- main.py
+++ main.py
@@ -5,36 +5,6 @@ from DivisionBit import BitImg
 from WaterMark import SimpleWaterMarkSet
 from WaterMark import SimpleWaterMarkReset
 
-def main_test():
-    base_size = 100
-    
-    img1 = Image.open("data/Lenna.png").convert("L").resize((base_size*4,base_size*4))
-    img_array1 = np.asanyarray(img1)
-    img2 = Image.open("data/idol_fan_penlight_men.png").convert("L").resize((base_size, base_size))
-    img_array2 = np.asanyarray(img2)
-    BitImg1 = DivisionBit(img_array1)
-    BitImg1.deivide()
-    BitImg2 = DivisionBit(img_array2)
-    BitImg2.deivide()
-    BitImg2.set_flat()
-    BitImg1.change(BitImg2,0)
-    #BitImg1.reset_flat()
-    BitImg1.flat2array()
-    BitImg1.save("result/test.png")
-    #BitImg1.deivide()
-    #BitImg1.show_flat(0)
-    #BitImg1.show()
-    img_r = Image.open("result/test.png").convert("L")
-    img_r.show()
-    img_r_array = np.asarray(img_r)
-#    img_r.show()
-    R = DivisionBit(img_r_array)
-    R.deivide()
-    #R.show_flat(0)
-    #R.show_flat_all()
-    BI = BitImg(R.flat[0])
-    BI.create_img(base_size,base_size)
-
 def create_data(fname):
     img2 = Image.open("data/"+fname).convert("L")
     img_array2 = np.asanyarray(img2)
@@ -58,5 +28,4 @@ def main():
     
 
 if __name__ == "__main__":
-    #main_test()
     main()
