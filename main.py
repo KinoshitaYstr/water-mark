1ee15018cd77791165efcd45a37a3bac1adf3714
KinoshitaYstr <control.division.cyber.crime@gmail.com>
2017-12-17 15:57:14 +0900

    オタクの中に外国人女性を入れることもできた

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from DivisionBit import DivisionBit
 from DivisionBit import BitImg
 
 def main():
-    base_size = 50
-    img1 = Image.open("data/Lenna.png").convert("L").resize((base_size*4,base_size*4))
+    base_size = 100
+    img1 = Image.open("data/idol_fan_penlight_men.png").convert("L").resize((base_size*4,base_size*4))
     img_array1 = np.asanyarray(img1)
-    img2 = Image.open("data/idol_fan_penlight_men.png").convert("L").resize((base_size,base_size))
+    img2 = Image.open("data/Lenna.png").convert("L").resize((base_size,base_size))
     img_array2 = np.asanyarray(img2)
     BitImg1 = DivisionBit(img_array1)
     BitImg1.deivide()
@@ -18,7 +18,7 @@ def main():
     BitImg1.save("result/test.png")
     BitImg1.show()
     BI = BitImg(BitImg1.flat[0])
-    BI.create_img(50,50)
+    BI.create_img(base_size,base_size)
     
 if __name__ == "__main__":
     main()
